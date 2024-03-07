4fea1f834561c22db55b96e9814eaf0f6c1c1e1a
zxq <zhangxiaoquan@clgg.com>
2024-03-07 15:06:45 +0800

    feat:增加全局异常拦截

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
+import { AllExceptionsFilter } from './common/exceptions/base.exception.filter';
+import { HttpExceptionFilter } from './common/exceptions/http.exception.filter';
 
 /**
  * 启动NestJS应用并监听3000端口
@@ -17,6 +19,8 @@ async function bootstrap() {
   );
   // 统一响应体格式
   app.useGlobalInterceptors(new TransformInterceptor());
+  // 异常过滤器
+  app.useGlobalFilters(new AllExceptionsFilter(), new HttpExceptionFilter());
   // 接口版本化管理
   app.enableVersioning({
     prefix: '/api',
