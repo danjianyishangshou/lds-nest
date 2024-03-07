8ababf9a2459356ec21aad03b760d9a02b69e12f
zxq <zhangxiaoquan@clgg.com>
2024-03-07 15:01:39 +0800

    feat: 增加版本控制

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { VersioningType, VERSION_NEUTRAL } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
@@ -5,9 +6,23 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
+/**
+ * 启动NestJS应用并监听3000端口
+ */
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);,
-  new FastifyAdapter(),//使用festity替换express
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  // 接口版本化管理
+  app.enableVersioning({
+    prefix: '/api',
+    defaultVersion: [VERSION_NEUTRAL, '1', '2'],
+    // defaultVersion: '1',
+    type: VersioningType.URI,
+  });
+
+  await app.listen(3000);
 }
+
 bootstrap();
